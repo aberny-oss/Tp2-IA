73ac208fab6a8b8b699f2aff92995f0634741704
Alexis Berny <aberny@gaming.tech>
2026-01-28 14:35:21 +0100

    hgd

--- src/PvZ/main.cpp
+++ src/PvZ/main.cpp
@@ -12,7 +12,8 @@ int main()
 {
 	GameManager* pInstance = GameManager::Get();
 
-	pInstance->CreateWindow(1280, 720, "SampleScene", 60, sf::Color::Black);
+	std::cout << " jeux lancer " << std::endl;
+	//pInstance->CreateWindow(1280, 720, "SampleScene", 60, sf::Color::Black);
 
 	//pInstance->LaunchScene<SampleScene>();
 
